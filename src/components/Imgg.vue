e85f878a6199e68c20d2676ca9f891018c11b9bd
djdanielsecco <djdanielsecco@gmail.com>
2018-01-22 04:03:02 -0200

    dasdas

--- src/components/Imgg.vue
+++ src/components/Imgg.vue
@@ -16,9 +16,9 @@
 <script>
 	var firebase = require('firebase');
 
-	
-	
-	
+
+
+
 	export default {
 		name: 'imgg',
 		data() {
@@ -34,8 +34,65 @@
 				if (!files.length)
 					return;
 				this.createImage(files[0]);
-var file = e.target.files[0];
-                  var storageRef = firebase.storage().ref(file.name);
+				var file = e.target.files[0];
+var storageRef = firebase.storage().ref(file.name);
+
+				var metadata = {
+					contentType: 'image/jpeg'
+				};
+
+				// Upload file and metadata to the object 'images/mountains.jpg'
+				var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
+
+				// Listen for state changes, errors, and completion of the upload.
+				uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+					function(snapshot) {
+						// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+						var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+						console.log('Upload is ' + progress + '% done');
+						switch (snapshot.state) {
+							case firebase.storage.TaskState.PAUSED: // or 'paused'
+								console.log('Upload is paused');
+								break;
+							case firebase.storage.TaskState.RUNNING: // or 'running'
+								console.log('Upload is running');
+								break;
+						}
+					},
+					function(error) {
+
+						// A full list of error codes is available at
+						// https://firebase.google.com/docs/storage/web/handle-errors
+						switch (error.code) {
+							case 'storage/unauthorized':
+								// User doesn't have permission to access the object
+								break;
+
+							case 'storage/canceled':
+								// User canceled the upload
+								break;
+
+							
+
+							case 'storage/unknown':
+								// Unknown error occurred, inspect error.serverResponse
+								break;
+						}
+					},
+					function() {
+						// Upload completed successfully, now we can get the download URL
+						var downloadURL = uploadTask.snapshot.downloadURL;
+					firebase.database().ref('/data2/').set({
+					foto: downloadURL,
+					lat: 1231212,
+					long: 11123123123,
+					user: 5585558
+				});
+					});
+				////////////////////////////////////////////////////////////////////////////
+
+
+				/*var storageRef = firebase.storage().ref(file.name);
                   storageRef.put(file);
 				storageRef.getDownloadURL().then(function(url) {
                  console.log(url + '<-----------------');
@@ -46,7 +103,7 @@ var file = e.target.files[0];
 					lat: 1231212,
 					long: 11123123123,
 					user: 5585558
-				});
+				});*/
 			},
 			createImage(file) {
 				var image = new Image();
@@ -57,11 +114,11 @@ var file = e.target.files[0];
 					vm.image = e.target.result;
 				};
 				reader.readAsDataURL(file);
-				
-				 // use the Blob or File API
-					/*storageRef.put(reader).then(function(snapshot) {
-						console.log('Uploaded a blob or file!');
-					});*/
+
+				// use the Blob or File API
+				/*storageRef.put(reader).then(function(snapshot) {
+					console.log('Uploaded a blob or file!');
+				});*/
 				console.log(image.innerText + '<kkkkk>');
 
 			},
